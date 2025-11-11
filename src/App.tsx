acd115bf00faeb51e9ea9c0564f1a90a5d5104dd
mezzode <mezzode@users.noreply.github.com>
2025-11-10 21:54:16 -0800

    Add basic Nameplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import "./App.css";
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Nameplate from "./Nameplate";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Nameplate />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Nameplate.tsx
@@ -0,0 +1,4 @@
+import styles from "./Nameplate.module.css";
+
+const Nameplate = () => <div className={styles.nameplate}>mezzode</div>;
+export default Nameplate;
