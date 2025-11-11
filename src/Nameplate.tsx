49f44de54cd5a6974908d85f9ded77112480da73
mezzode <mezzode@users.noreply.github.com>
2025-11-10 21:54:37 -0800

    Add name toggling

--- src/Nameplate.tsx
+++ src/Nameplate.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
 import styles from "./Nameplate.module.css";
 
-const Nameplate = () => <div className={styles.nameplate}>mezzode</div>;
+const Nameplate = () => {
+  const [showAlt, setShowAlt] = useState<boolean>(false);
+  return (
+    <div className={styles.nameplate} onClick={() => setShowAlt(!showAlt)}>
+      {showAlt ? "Ash Bacal" : "mezzode"}
+    </div>
+  );
+};
 export default Nameplate;
