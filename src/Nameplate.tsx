6b359b23e6a407c7e9088d96aeaaf886d9c68b5e
mezzode <mezzode@users.noreply.github.com>
2025-11-10 23:30:16 -0800

    Add color switching

--- src/Nameplate.tsx
+++ src/Nameplate.tsx
@@ -4,7 +4,10 @@ import styles from "./Nameplate.module.css";
 const Nameplate = () => {
   const [showAlt, setShowAlt] = useState<boolean>(false);
   return (
-    <div className={styles.nameplate} onClick={() => setShowAlt(!showAlt)}>
+    <div
+      className={`${styles.nameplate} ${showAlt ? styles.alt : ""}`}
+      onClick={() => setShowAlt(!showAlt)}
+    >
       {showAlt ? "Ash Bacal" : "mezzode"}
     </div>
   );
