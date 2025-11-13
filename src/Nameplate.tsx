9fee0e3ad6be1678f756cc15abbbbbffa5fa43e3
mezzode <mezzode@users.noreply.github.com>
2025-11-12 17:19:01 -0800

    Add menu

--- src/Nameplate.tsx
+++ src/Nameplate.tsx
@@ -11,16 +11,31 @@ interface Props {
 const Nameplate = ({ alignment }: Props) => {
   const [showAlt, setShowAlt] = useState<boolean>(false);
   return (
-    <div
-      className={
-        clsx(styles.nameplate, styles[alignment], {
-          [styles.alt]: showAlt
-        })
-      }
-      onClick={() => setShowAlt(!showAlt)}
-    >
-      {showAlt ? "Ash Bacal" : "mezzode"}
+    <div className={clsx(styles.container, styles[alignment])}>
+      <div
+        className={clsx(styles.nameplate, {
+          [styles.alt]: showAlt,
+        })}
+        onClick={() => setShowAlt(!showAlt)}
+      >
+        {showAlt ? "Ash Bacal" : "mezzode"}
+      </div>
+      <Menu {...{ alignment }} />
     </div>
   );
 };
+
+function Menu({ alignment }: { alignment: Alignment }) {
+  return (
+    <div className={clsx(styles.menu, styles[alignment])}>
+      <svg className="bi" width="32" height="32" fill="currentColor">
+        <use href="node_modules/bootstrap-icons/bootstrap-icons.svg#envelope" />
+      </svg>
+      <svg className="bi" width="32" height="32" fill="currentColor">
+        <use href="node_modules/bootstrap-icons/bootstrap-icons.svg#github" />
+      </svg>
+    </div>
+  );
+}
+
 export default Nameplate;
