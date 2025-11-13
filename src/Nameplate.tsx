956c2f41e1b4774121b4d132fc01852b5b4bc6fb
mezzode <mezzode@users.noreply.github.com>
2025-11-12 17:25:32 -0800

    Add href support to Icon component

--- src/Nameplate.tsx
+++ src/Nameplate.tsx
@@ -28,18 +28,29 @@ function Nameplate({ alignment }: Props) {
 function Menu({ alignment }: { alignment: Alignment }) {
   return (
     <div className={clsx(styles.menu, styles[alignment])}>
-      <Icon icon="envelope" />
-      <Icon icon="github" />
+      <Icon icon="envelope" href="mailto:mezzode@mezzode.com" />
+      <Icon icon="github" href="https://github.com/mezzode" />
+      <Icon icon="linkedin" href="https://www.linkedin.com/in/mezzode/" />
     </div>
   );
 }
 
-function Icon({ icon }: { icon: string }) {
-  return (
+function Icon({ icon, href }: { icon: string; href?: string }) {
+  const svg = (
     <svg className="bi" width="32" height="32" fill="currentColor">
       <use href={`node_modules/bootstrap-icons/bootstrap-icons.svg#${icon}`} />
     </svg>
   );
+
+  if (href) {
+    return (
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        {svg}
+      </a>
+    );
+  } else {
+    return svg;
+  }
 }
 
 export default Nameplate;
