14d73957ef8b063804f23ea3c4c0a08c1466b318
mezzode <mezzode@users.noreply.github.com>
2025-11-12 16:50:11 -0800

    Support multiple alignments

--- src/Nameplate.tsx
+++ src/Nameplate.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
+import clsx from "clsx";
 import styles from "./Nameplate.module.css";
 
-const Nameplate = () => {
+type Alignment = "left" | "center" | "right";
+
+interface Props {
+  alignment: Alignment;
+}
+
+const Nameplate = ({ alignment }: Props) => {
   const [showAlt, setShowAlt] = useState<boolean>(false);
   return (
     <div
-      className={`${styles.nameplate} ${showAlt ? styles.alt : ""}`}
+      className={
+        clsx(styles.nameplate, styles[alignment], {
+          [styles.alt]: showAlt
+        })
+      }
       onClick={() => setShowAlt(!showAlt)}
     >
       {showAlt ? "Ash Bacal" : "mezzode"}
