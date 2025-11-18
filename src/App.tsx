240414cff050014803d93c624c6c64afdb0545af
mezzode <mezzode@users.noreply.github.com>
2025-11-18 01:09:15 -0800

    Fix theming

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import styles from "./App.module.css";
 import Grid from "./Grid";
 
 function App() {
+  
+  const primaryColor = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--primary-color");
+  const secondaryColor = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--secondary-color");
+
   return (
     <div className={styles.app}>
       <Nameplate alignment="right" />
-      <Grid />
+      <Grid color={primaryColor} />
     </div>
   );
 }
