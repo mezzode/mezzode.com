15a1d3679e4757d3237e5219227b10e129ed9099
mezzode <mezzode@users.noreply.github.com>
2025-11-21 18:06:42 -0800

    Move scheme code to own directory

--- src/store.ts
+++ src/store.ts
@@ -1,51 +1,5 @@
-import {
-  configureStore,
-  createListenerMiddleware,
-  createSlice,
-  type PayloadAction,
-} from "@reduxjs/toolkit";
-
-const initialMode =
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
-
-type Mode = "light" | "dark";
-
-const initialPrimaryColor = getComputedStyle(
-  document.documentElement
-).getPropertyValue("--primary-color");
-const initialSecondaryColor = getComputedStyle(
-  document.documentElement
-).getPropertyValue("--secondary-color");
-
-const schemeSlice = createSlice({
-  name: "scheme",
-  initialState: {
-    mode: initialMode as Mode,
-    primaryColor: initialPrimaryColor,
-    secondaryColor: initialSecondaryColor,
-  },
-  reducers: {
-    setDarkMode: (state) => {
-      state.mode = "dark";
-      state.primaryColor = "var(--light)";
-      state.secondaryColor = "var(--dark)";
-    },
-    setLightMode: (state) => {
-      state.mode = "light";
-      state.primaryColor = "var(--dark)";
-      state.secondaryColor = "var(--light)";
-    },
-    setPrimaryColor: (state, action: PayloadAction<string>) => {
-      state.primaryColor = action.payload;
-    },
-    setSecondaryColor: (state, action: PayloadAction<string>) => {
-      state.secondaryColor = action.payload;
-    },
-  },
-});
+import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
+import schemeSlice from "./Schemer/schemeSlice";
 
 export const listener = createListenerMiddleware<RootState>();
 
